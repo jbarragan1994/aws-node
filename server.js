e05cebe54f91f4886d2126aff06a33ba7a976291
Barragan <jcbarragan1994@gmail.com>
2024-11-22 21:01:27 -0400

    message

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send('hello world express')
+    res.send('<h1>Express in h1</h1>')
 })
-app.listen(3000)
 
-console.log(`server on port 3000`);
+app.listen(3000, () => {
+    console.log("Server is running on http://0.0.0.0:3000");
+});
