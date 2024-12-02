f24cb22e71f93429c74bed76ac970f07c7a8d074
Barragan <jcbarragan1994@gmail.com>
2024-12-02 12:14:45 -0400

    change port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Hola Yesikitaaa</h1>')
 })
 
-app.listen(8080, () => {
+app.listen(3000, () => {
     console.log("Server is running on http://0.0.0.0:8080");
 });
